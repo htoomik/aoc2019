961f65dd3764efddabba08681e63bddae3159ef9
Helen Toomik <helen.toomik@tretton37.com>
2019-12-17 22:56:10 +0100

    Day 6 part 2

--- Code/Day06.cs
+++ Code/Day06.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace aoc2019
 {
@@ -8,15 +7,16 @@ namespace aoc2019
     {
         public int Solve(List<string> lines)
         {
-            var links = new Dictionary<string, Orbit>();
-            foreach (var line in lines)
-            {
-                var parts = line.Split(")");
-                var orbitsAroundCom = parts[0] == "COM";
-                var orbit = new Orbit(parts[1], parts[0], orbitsAroundCom ? 1 : 0);
-                links.Add(parts[1], orbit);
-            }
+            var links = Parse(lines);
 
+            CalculateDistances(links);
+
+            var sum = links.Values.Sum(v => v.Distance);
+            return sum;
+        }
+
+        private static void CalculateDistances(Dictionary<string, Orbit> links)
+        {
             var workToDo = true;
             while (workToDo)
             {
@@ -32,9 +32,68 @@ namespace aoc2019
                         planet.Distance = orbitsAround.Distance + 1;
                 }
             }
+        }
 
-            var sum = links.Values.Sum(v => v.Distance);
-            return sum;
+        public int Solve2(List<string> lines)
+        {
+            var links = Parse(lines);
+            CalculateDistances(links);
+
+            var santa = links["SAN"];
+            var you = links["YOU"];
+
+            var santaPath = GetPath(santa, links);
+            var yourPath = GetPath(you, links);
+
+            var intersect = FindFirstSharedOrbit(santaPath, yourPath);
+            var intersection = links[intersect];
+
+            var santaDistanceToIntersection = santa.Distance - intersection.Distance;
+            var yourDistanceToIntersection = you.Distance - intersection.Distance;
+
+            var santaDistanceToYou = santaDistanceToIntersection + yourDistanceToIntersection;
+            var moves = santaDistanceToYou - 2;
+
+            return moves;
+        }
+
+        private static Dictionary<string, Orbit> Parse(List<string> lines)
+        {
+            var links = new Dictionary<string, Orbit>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(")");
+                var orbitsAroundCom = parts[0] == "COM";
+                var orbit = new Orbit(parts[1], parts[0], orbitsAroundCom ? 1 : 0);
+                links.Add(parts[1], orbit);
+            }
+
+            return links;
+        }
+
+        private static List<string> GetPath(Orbit orbit, Dictionary<string, Orbit> links)
+        {
+            var result = new List<string>();
+            var current = orbit;
+
+            while (true)
+            {
+                var currentOrbitsAround = current.OrbitsAround;
+
+                result.Add(currentOrbitsAround);
+
+                if (currentOrbitsAround == "COM")
+                    break;
+
+                current = links[currentOrbitsAround];
+            }
+
+            return result;
+        }
+
+        private static string FindFirstSharedOrbit(List<string> santaPath, List<string> yourPath)
+        {
+            return santaPath.First(yourPath.Contains);
         }
 
         private class Orbit
--- Test/Test06.cs
+++ Test/Test06.cs
@@ -42,5 +42,37 @@ K)L".Trim().Split("\r\n").ToList();
             var result = solver.Solve(data);
             _testOutputHelper.WriteLine(result.ToString());
         }
+
+        [Fact]
+        public void Part2()
+        {
+            var data = @"
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN".Trim().Split("\r\n").ToList();
+            var solver = new Day06();
+            var result = solver.Solve2(data);
+            result.ShouldBe(4);
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var data = InputDataHelper.GetLines(6);
+            var solver = new Day06();
+            var result = solver.Solve2(data);
+            _testOutputHelper.WriteLine(result.ToString());
+        }
+
     }
 }
